d10bd44a36d2ab5fcce2223331645fdcec8ec4a3
Stefan Winterberger <stefanwinterb88@hotmail.com>
2015-11-23 13:00:04 +0100

    Tutorial till Part 4

---
+++ Assets/Code/ClickOnFaceScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickOnFaceScript : MonoBehaviour
+{
+
+    public Vector3 delta;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // This function is triggered when the mouse cursor is over the GameObject on which this script runs
+    void OnMouseOver()
+    {
+        // If the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Display a message in the Console tab
+            Debug.Log("Left click!");
+            // Destroy the parent of the face we clicked
+            Destroy(this.transform.parent.gameObject);
+        }
+
+        // If the right mouse button is pressed
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Display a message in the Console tab
+            Debug.Log("Right click!");
+
+            // Call method from WorldGenerator class
+            WorldGenerator.CloneAndPlace(this.transform.parent.transform.position + delta, // N = C + delta
+                                         this.transform.parent.gameObject); // The parent GameObject 
+        }
+    }
+}
---
+++ Assets/Code/WorldGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGenerator : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void CloneAndPlace(Vector3 newPosition, GameObject originalGameobject)
+    {
+        // Clone
+        GameObject clone = (GameObject)Instantiate(originalGameobject, newPosition, Quaternion.identity);
+        // Place
+        clone.transform.position = newPosition;
+        // Rename
+        clone.name = "Voxel@" + clone.transform.position;
+    }
+}
